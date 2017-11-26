95d46be49a7275da214fb557f7fdc9d3ee405d20
Victor Enrico <victorenrico50@gmail.com>
2017-11-26 01:17:24 -0200

    area professor criada

--- usuario/views.py
+++ usuario/views.py
@@ -34,3 +34,23 @@ def area_aluno_smart_class1(request):
 @user_passes_test(checa_aluno, login_url='/?error=acesso', redirect_field_name = None)
 def area_aluno_smart_class2(request):
     return render(request, "areaAlunoSmartClass2.html")
+
+#Area Professor
+
+def area_professor(request):
+    return render(request, "areaProfessor.html")
+
+def area_professor_boletim(request):
+    return render(request, "areaProfessorBoletim.html")
+
+def area_professor_contato_professor(request):
+    contexto = {
+        'professores': Professor.objects.all()
+    }
+    return render(request, "areaProfessorContatoProfessor.html", contexto)
+
+def area_professor_smart_class1(request):
+    return render(request, "areaProfessorSmartClass1.html")
+
+def area_professor_smart_class2(request):
+    return render(request, "areaProfessorSmartClass2.html")
