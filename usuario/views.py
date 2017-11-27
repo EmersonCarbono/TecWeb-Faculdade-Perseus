57a118408a0fe57918a7ce2f15cc280c6d56d3ad
Gabriel Leal <gabbrielleal@icloud.com>
2017-11-27 10:26:21 -0200

    Update views.py
    implementei a função teste_onlinne para retornar a página testeOnline.html

--- usuario/views.py
+++ usuario/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
 
-from .models import Professor
-
 # Create your views here.
 def checa_aluno(user):
      return user.perfil == 'A'
@@ -20,10 +18,7 @@ def area_aluno_boletim(request):
 @login_required(login_url='/entrar')
 @user_passes_test(checa_aluno, login_url='/?error=acesso', redirect_field_name = None)
 def area_aluno_contato_professor(request):
-    contexto = {
-        'professores': Professor.objects.all()
-    }
-    return render(request, "areaAlunoContatoProfessor.html", contexto)
+    return render(request, "areaAlunoContatoProfessor.html")
 
 @login_required(login_url='/entrar')
 @user_passes_test(checa_aluno, login_url='/?error=acesso', redirect_field_name = None)
@@ -35,28 +30,7 @@ def area_aluno_smart_class1(request):
 def area_aluno_smart_class2(request):
     return render(request, "areaAlunoSmartClass2.html")
 
-#Area Professor
-
-def area_professor(request):
-    return render(request, "areaProfessor.html")
-
-def area_professor_boletim(request):
-    return render(request, "areaProfessorBoletim.html")
-
-def area_professor_contato_professor(request):
-    contexto = {
-        'professores': Professor.objects.all()
-    }
-    return render(request, "areaProfessorContatoProfessor.html", contexto)
-
-def area_professor_smart_class1(request):
-    return render(request, "areaProfessorSmartClass1.html")
-
-def area_professor_smart_class2(request):
-    return render(request, "areaProfessorSmartClass2.html")
-
-def area_professor_adicionar_material(request):
-    return render(request, "areaProfessorAdicionarMaterial.html")
-
-def area_professor_adicionar_boletim(request):
-    return render(request, "areaProfessorAdicionarBoletim.html")
+@login_required(login_url='/entrar')
+@user_passes_test(checa_aluno, login_url='/?error=acesso', redirect_field_name = None)
+def teste_online(request):
+    return render(request, "testeOnline.html")
