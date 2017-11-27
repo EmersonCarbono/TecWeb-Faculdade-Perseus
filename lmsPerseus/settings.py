76232861814f2c9ded5a0d48cb3ba51bc1ae9cdb
Victor Enrico <victorenrico50@gmail.com>
2017-11-27 14:56:21 -0200

    banco sql ativado

--- lmsPerseus/settings.py
+++ lmsPerseus/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'lmsPerseus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'sql_server.pyodbc',
         'NAME': 'faculdade-perseus',
@@ -84,15 +84,15 @@ WSGI_APPLICATION = 'lmsPerseus.wsgi.application'
         'PASSWORD': 'Faculdade123',
         'HOST': 'faculdade-perseus.database.windows.net',
     },
-}'''
-
+}
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
 
 
 # Password validation
